83a39fc93009e774f887b607678f0da682310e0d
Shashini <alaganmahalingam@Alagans-iMac.local>
2019-09-24 11:40:38 +0530

    define user roles

--- app/screens/Search/searchBarScreen.js
+++ app/screens/Search/searchBarScreen.js
@@ -133,14 +133,7 @@ export default class SearchBarScreen extends Component {
                 console.log(arr.length);
 
             }
-            // arr.splice(arr.length - 1, 1);
-            // console.log(arr.length);
-            // }
-
-            // arr = [
-            //     location,
-            //     ...arr
-            // ];
+        
             arr = [
                 {
                     description: location.description,
@@ -151,11 +144,6 @@ export default class SearchBarScreen extends Component {
 
             this.state.recentSearchList = arr;
             console.log('this.state.recentSearchList', this.state.recentSearchList);
-
-            // for (const i in this.state.recentSearchList) {
-            //     const loc = this.state.recentSearchList[i];
-            //     console.log('loc.name', loc.name);
-            // }
 
             await AsyncStorage.setItem('@LocationSearchList', JSON.stringify(arr));
 
